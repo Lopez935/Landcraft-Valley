3eb5cb5f3d60352b3797785790b0950a5c915eb9
jorge <jorgegarza.its@gmail.com>
2020-07-10 15:18:41 -0500

    Diseño de imagenes de la galeria

--- galeria.js
+++ galeria.js
@@ -4,45 +4,45 @@ const listaFotos = document.getElementById('contentGlobal');
 (function(){
     var fotos = [
         {
-            fecha: '',
-            img1: '',
-            img2: '',
-            img3: '',
-            img4: '',
+            fecha: '28 de agosto del 2020',
+            img1: 'image/imgPrueba1.jpg',
+            img2: 'image/imgPrueba2.jpg',
+            img3: 'image/imgPrueba3.jpg',
+            img4: 'image/imgPrueba4.png',
         },
         {
-            fecha: '',
-            img1: '',
-            img2: '',
-            img3: '',
-            img4: '',
+            fecha: '22 de agosto del 2020',
+            img1: 'image/imgPrueba5.jpg',
+            img2: 'image/imgPrueba6.jpg',
+            img3: 'image/imgPrueba7.jpg',
+            img4: 'image/imgPrueba8.jpg',
         },
         {
-            fecha: '',
-            img1: '',
-            img2: '',
-            img3: '',
-            img4: '',
+            fecha: '15 de agosto del 2020',
+            img1: 'image/imgPrueba9.jpg',
+            img2: 'image/imgPrueba10.jpg',
+            img3: 'image/imgPrueba11.jpg',
+            img4: 'image/imgPrueba12.jpg',
         },
     ];
     
     fotos.forEach(function(foto, index){
         const h2Foto = document.createElement('h2');
-        h2Foto.innerHTML = `26 de agosto 2020`;
+        h2Foto.innerHTML = `${foto.fecha}`;
 
         const divFoto = document.createElement('div');
         divFoto.innerHTML = `
-            <a href="" src="#image1">
-                <img src="image/example.png" alt="foto"/>
+            <a href="#image1">
+                <img src="${foto.img1}" alt="foto"/>
             </a>
-            <a href="" src="#image1">
-                <img src="image/example.png" alt="foto"/>
+            <a href="#image2">
+                <img src="${foto.img2}" alt="foto"/>
             </a>
-            <a href="" src="#image1">
-                <img src="image/example.png" alt="foto"/>
+            <a href="#image3">
+                <img src="${foto.img3}" alt="foto"/>
             </a>
-            <a href="" src="#image1">
-                <img src="image/example.png" alt="foto"/>
+            <a href="#image4">
+                <img src="${foto.img4}" alt="foto"/>
             </a>
         `;
         listaFotos.appendChild(h2Foto);
